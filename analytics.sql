5cfd31c38d96a86a881598e3c286b1159f4f17bf
SofiaMPinho <93188437+SofiaMPinho@users.noreply.github.com>
2022-06-22 12:13:05 +0100

    Update analytics.sql

---
+++ analytics.sql
@@ -0,0 +1,17 @@
+/*Número total de artigos vendidos num dado período (i.e. entre duas datas), 
+por dia da semana, por concelho e no total*/
+
+SELECT dia_semana, concelho, SUM(unidades)
+FROM Vendas
+WHERE 
+GROUP BY ROLLUP(dia_semana, concelho)
+ORDER BY dia_semana, concelho; 
+
+/*Número total de artigos vendidos num dado distrito (i.e. “Lisboa”), 
+por concelho, dia da semana e no total*/
+
+SELECT concelho, cat, SUM(unidades)
+FROM Vendas
+WHERE distrito = ‘Lisboa’
+GROUP BY ROLLUP(concelho, cat)
+ORDER BY concelho, cat;
