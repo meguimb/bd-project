0b4c09a5b0b9688795c2d80b5627474ef191dd7d
SofiaMPinho <93188437+SofiaMPinho@users.noreply.github.com>
2022-06-22 12:17:43 +0100

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,7 +1,15 @@
 CREATE VIEW Vendas(ean, cat, ano, trimestre, dia_mes, dia_semana, distrito, concelho, unidades)
 AS
-SELECT produto.ean, categoria.nome, EXTRACT(QUARTER FROM “timestamps”), EXTRACT(DAY FROM “timestamps”), 
-EXTRACT(DOW FROM “timestamps”), pr.distrito, pr.concelho, er.unidades
+SELECT 
+  produto.ean, 
+  categoria.nome, 
+  EXTRACT(YEAR FROM er.instance), 
+  EXTRACT(QUARTER FROM er.instance), 
+  EXTRACT(DAY FROM er.instance), 
+  EXTRACT(DOW FROM er.instance), 
+  pr.distrito, 
+  pr.concelho, 
+  er.unidades
 FROM produto
 INNER JOIN categoria ON (produto.cat = categoria.nome)
 INNER JOIN evento_reposicao AS er ON (produto.ean = er.ean)
